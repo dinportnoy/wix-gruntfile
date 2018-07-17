835c88cb0e89d983e8a795422b60b2c63d99bf8f
Aviad Hadad <aviadh@wix.com>
2018-07-17 16:00:25 +0300

    ugly temp workaround for quix

--- grunt-sections/connect.js
+++ grunt-sections/connect.js
@@ -100,6 +100,7 @@ module.exports = function (grunt, options) {
             mountFolder(connect, 'node_modules'),
             proxyFolder('/wcservices/', '<%= yeoman.api %>'.replace('_api', 'wcservices')),
             proxyFolder('/_api/', '<%= yeoman.api %>'),
+            proxyFolder('/quix/', '<%= yeoman.api %>'),
             proxyFolder('/_partials/', '<%= yeoman.partials %>'),
             proxyFolder('/_livereload/', 'http://localhost:<%= connect.options.livereload %>/'),
             connect.urlencoded()
@@ -150,6 +151,7 @@ module.exports = function (grunt, options) {
             mountFolder(connect, 'dist-angular'),
             mountFolder(connect, 'dist'),
             proxyFolder('/_api/', '<%= yeoman.api %>'),
+            proxyFolder('/quix/', '<%= yeoman.api %>'),
             proxyFolder('/_partials/', '<%= yeoman.partials %>'),
             connect.urlencoded()
           ]).concat(getProxies('proxies'));
